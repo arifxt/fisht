2427fa4d3d3523be4aefbb846d9b6952be359afc
arif <arifxtry@gmail.com>
2026-03-11 01:39:47 +0700

    Update d.sh

--- d.sh
+++ d.sh
@@ -4,4 +4,3 @@ wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite
 wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2002-2.710.707.apk
 wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2003-2.710.707.apk
 wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2004-2.710.707.apk
-mv *.apk ~/storage/shared/Download/.
