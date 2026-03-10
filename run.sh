01fc37e51ac41e5bca138c8185b8eaed6c60b28c
arif <arifxtry@gmail.com>
2026-03-11 01:29:40 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #/bin/bash!
-
-
+#Download APK
 wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2001-2.710.707.apk
 wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2002-2.710.707.apk
 wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2003-2.710.707.apk
 wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2004-2.710.707.apk
-#Install all
+#Install ALL APK
 install_all() {
   echo "🔍 Mencari file .apk..."
   for APK in ./*.apk; do
