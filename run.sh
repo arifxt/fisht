73e8fff83d26c163489ebd6b6714eded4e38ad9b
arif <arifxtry@gmail.com>
2026-03-11 01:18:03 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#/bin/bash!
+pkg install wget -y
+wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2001-2.710.707.apk
+wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2002-2.710.707.apk
+wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2003-2.710.707.apk
+wget https://cloud.rnt-team.me/public.php/dav/files/8yceNR6dTkgYH7n/Delta%20Lite/v2.710.707-02/Android%2010/Lite%2004-2.710.707.apk
